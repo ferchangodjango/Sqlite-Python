927454f9731777b89d41bbf481305400ca6e76cd
FerchangoDjango <dacofercho@gmail.com>
2024-03-23 06:21:25 +0100

    Agregando 2 Querys al archivo de conexión

--- UDF.py
+++ UDF.py
@@ -1,21 +1,44 @@
 import sqlite3
 import pandas as pd
 
+#Realizando conección con python
 square=lambda n: n*n
 try:
     with sqlite3.connect('E:/DB SQL3/DATA_BASE_NORTHWIND/Northwind.db') as comn:
-        comn.create_function('Square',1,square)
         cursor=comn.cursor()
         cursor.execute("""
-                    SELECT *,Square(Price)
-                    FROM Products""")
+                    SELECT P.ProductID,P.ProductName,Price*Quantity AS Profit
+                    FROM OrderDetails AS OD
+                    INNER JOIN Products AS P ON OD.ProductID=P.ProductID
+                    GROUP BY OD.ProductID
+                    ORDER BY Profit DESC
+                    LIMIT 10"""
+                       )
         result=cursor.fetchall()
         results_DF=pd.DataFrame(result)
     
-    print(results_DF)
+    with sqlite3.connect('E:/DB SQL3/DATA_BASE_NORTHWIND/Northwind.db') as comn2:
+        cursor2=comn2.cursor()
+        cursor2.execute("""
+                        SELECT Employees.FirstName|| " " || Employees.LastName AS FullName,Products.Price,OrderDetails.Quantity,Products.Price*OrderDetails.Quantity AS TotalProfit
+                        FROM OrderDetails 
+                        INNER JOIN Orders ON Orders.OrderID=OrderDetails.OrderID
+                        INNER JOIN Products ON OrderDetails.ProductID=Products.ProductID
+                        INNER JOIN Employees ON Employees.EmployeeID=Orders.EmployeeID
+                        GROUP BY FullName 
+                        ORDER BY TotalProfit DESC
+                        """
+                       )
+        result2=cursor2.fetchall()
+        results2_DF=pd.DataFrame(result2)
+    #print(results_DF)
 
 except sqlite3.Error as e:
     print(f"Error en la base de datos: {e}")
 
 finally:
     comn.close()
+    comn2.close()
+
+
+print(results_DF)
